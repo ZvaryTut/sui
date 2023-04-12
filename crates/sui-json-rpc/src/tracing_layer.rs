72fbe6e23abcf10e8e18a69824d4cc6f285e46a7
Will Yang <willyang@mystenlabs.com>
2023-04-12 11:12:57 -0400

    copyrights

--- crates/sui-json-rpc/src/tracing_layer.rs
+++ crates/sui-json-rpc/src/tracing_layer.rs
@@ -1,3 +1,6 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
 use hyper::{header::HeaderValue, Request, Response};
 use std::task::{Context, Poll};
 use tower::Layer;
