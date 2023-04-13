76e7cf89e97f3a7fc438886a1f360e6125f23d77
github-action-benchmark <github@users.noreply.github.com>
2023-04-13 17:03:53 +0000

    add Benchmark (cargo) benchmark result for 02b64f08abd5bdf48b08f512bea34f38a325e745

--- bench/indexer/data.js
+++ bench/indexer/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1681404648768,
+  "lastUpdate": 1681405432487,
   "repoUrl": "https://github.com/MystenLabs/sui",
   "entries": {
     "Benchmark": [
@@ -35,6 +35,42 @@ window.BENCHMARK_DATA = {
             "name": "get_checkpoint",
             "value": 519648,
             "range": "± 40330",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "patrickkuo@me.com",
+            "name": "Patrick Kuo",
+            "username": "patrickkuo"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "02b64f08abd5bdf48b08f512bea34f38a325e745",
+          "message": "[bugfix] - balance changes generation should not ignore missing objects as old object can be pruned in FN (#10657)\n\n## Description \r\n\r\nfullnode can return incorrect balance changes for historic transaction\r\nbecause of pruning, the balance changes generation logic incorrectly\r\nignore missing object, this PR fixes that and will return error when a\r\nobject needed for balance change generation is not available.\r\n\r\n---------\r\n\r\nCo-authored-by: healthydeve <jian@mystenlabs.com>",
+          "timestamp": "2023-04-13T17:53:53+01:00",
+          "tree_id": "54f9b9a957ba7b86f6e7af6968887eedaf37136c",
+          "url": "https://github.com/MystenLabs/sui/commit/02b64f08abd5bdf48b08f512bea34f38a325e745"
+        },
+        "date": 1681405428605,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "persist_checkpoint",
+            "value": 193101857,
+            "range": "± 12375225",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "get_checkpoint",
+            "value": 545476,
+            "range": "± 38538",
             "unit": "ns/iter"
           }
         ]
